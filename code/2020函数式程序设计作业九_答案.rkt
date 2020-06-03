957e07594a4103d10537b3a5726a4f111091df50
magic3007 <maijing3007@qq.com>
2020-06-04 00:33:23 +0800

    update

---
+++ code/2020函数式程序设计作业九_答案.rkt
@@ -0,0 +1,376 @@
+****1: 流函数 partial-sums
+by 1800012945
+#lang racket
+;book ex 3.55
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;取 stream里面第 n 项,n从0开始算
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+ (define (partial-sums op s) ;以s为参数,返回的流是 s0,(op s0 s1),(op s0 s1 s2), ....
+   (define (iter sum next)
+     (cons-stream sum (iter (op sum (stream-car next)) (stream-cdr next))))
+   (iter (stream-car s) (stream-cdr s)))
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+by 1800013002
+#lang racket
+;book ex 3.55
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;取 stream里面第 n 项,n从0开始算
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+ (define (partial-sums op s) ;以s为参数,返回的流是 s0,(op s0 s1),(op s0 s1 s2), ....
+   (let ((s0 (stream-ref s 0))
+         (s1 (stream-ref s 1))
+         (ss (stream-cdr (stream-cdr s))))
+     (cons-stream s0 (partial-sums op (cons-stream (op s0 s1) ss)))))
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+by 1700012966
+#lang racket
+;book ex 3.55
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;取 stream里面第 n 项,n从0开始算
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+ (define (partial-sums op s) ;以s为参数,返回的流是 s0,(op s0 s1),(op s0 s1 s2), ....
+(define (op-map s1 s2)
+     (cons-stream (op (stream-car s1) (stream-car (stream-cdr s2))) (op-map (stream-cdr s1) (stream-cdr s2))))
+   (cons-stream (stream-car s) (op-map (partial-sums op s) s)))
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+by 1800012901
+#lang racket
+;book ex 3.55
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;取 stream里面第 n 项,n从0开始算
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+ (define (partial-sums op s) ;以s为参数,返回的流是 s0,(op s0 s1),(op s0 s1 s2), ....
+	 (define (gao op v s)
+	 	(cons-stream
+	 		(op v (stream-car s))
+	 		(gao op (op v (stream-car s)) (stream-cdr s))
+	 	)
+	 )
+	 (cons-stream
+	 	(stream-car s)
+	 	(gao op (stream-car s) (stream-cdr s))
+	 )
+)
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+****2: 流函数partial-sums-ex
+by 1800012945
+#lang racket
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;get the nth item from s. n starts from 
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+(eval '(define (display-stream s n) ;display first n items of s
+  (if (= n 0)
+      (displayln "")
+      (begin (display (stream-car s)) (display " ") (display-stream (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+(define (partial-sums-ex op s)
+  (define (iter sum1 sum2 next)
+    (cons-stream sum1 (iter sum2 (op sum1 (stream-car next)) (stream-cdr next))))
+  (iter (stream-car s) (stream-car (stream-cdr s)) (stream-cdr (stream-cdr s))))
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+by 1800013002
+#lang racket
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;get the nth item from s. n starts from 
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+(eval '(define (display-stream s n) ;display first n items of s
+  (if (= n 0)
+      (displayln "")
+      (begin (display (stream-car s)) (display " ") (display-stream (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+(define (partial-sums-ex op s)
+  (let ((s0 (stream-car s))
+        (s1 (stream-car (stream-cdr s)))
+        (s2 (stream-car (stream-cdr (stream-cdr s))))
+        (ss (stream-cdr (stream-cdr (stream-cdr s)))))
+    (cons-stream s0 (partial-sums-ex op (cons-stream s1 (cons-stream (op s0 s2) ss))))))
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+by 1700012966
+#lang racket
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;get the nth item from s. n starts from 
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+(eval '(define (display-stream s n) ;display first n items of s
+  (if (= n 0)
+      (displayln "")
+      (begin (display (stream-car s)) (display " ") (display-stream (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+(define (partial-sums-ex op s)
+(define (op-map s1 s2)
+     (cons-stream (op (stream-car s1) (stream-car (stream-cdr (stream-cdr s2)))) (op-map (stream-cdr s1) (stream-cdr s2))))
+   (cons-stream (stream-car s) (cons-stream (stream-car (stream-cdr s))
+                (op-map (partial-sums-ex op s) s))))
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
+by 1800012901
+#lang racket
+(require r5rs)
+(define env (scheme-report-environment 5))
+(eval '(define (stream-car stream) (car stream)) env)
+(eval '(define (stream-cdr stream) (force (cdr stream))) env)
+(eval '(define-syntax cons-stream
+  (syntax-rules ()
+    [(cons-stream x y) (cons x (delay y))])) env)
+
+(eval '(define the-empty-stream '()) env)
+(eval '(define (stream-null? stream) (null? stream)) env)
+         
+(eval '(define (stream-ref s n)  ;get the nth item from s. n starts from 
+  (if (stream-null? s) the-empty-stream
+      (if (= n 0)
+          (stream-car s)
+          (stream-ref (stream-cdr s) (- n 1)))))
+      env)
+
+(eval '(define (display-stream s n) ;display first n items of s
+  (if (= n 0)
+      (displayln "")
+      (begin (display (stream-car s)) (display " ") (display-stream (stream-cdr s) (- n 1)))))
+      env)
+
+
+(eval '
+(define (partial-sums-ex op s)
+		(define (gao op v1 v2 s)
+		 	(cons-stream
+		 		(op v1 (stream-car s))
+		 		(cons-stream
+		 			(op v2 (stream-car (stream-cdr s)))
+		 			(gao op (op v1 (stream-car s)) (op v2 (stream-car (stream-cdr s))) (stream-cdr (stream-cdr s)))
+		 		)
+		 	)
+	 	)
+		(cons-stream
+			(stream-car s)
+			(cons-stream
+				(stream-car (stream-cdr s))
+				(gao op (stream-car s) (stream-car (stream-cdr s)) (stream-cdr (stream-cdr s)))
+			)
+		)
+)
+env)
+
+(define (myloop)
+  (define (eval-codes codes last-val)
+    (if (null? codes)
+        last-val
+        (eval-codes (cdr codes) (eval (car codes) env))))
+    
+  (let ((codes (read)))
+    (if (eq? codes eof)
+        (void)
+        (begin (displayln (eval-codes codes (void))) (myloop)))))
+(myloop)
+
